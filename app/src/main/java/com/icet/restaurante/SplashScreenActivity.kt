5206e52faf85d0860cf1494ea4453729b3e6685f
Felipe V Assunção <felippevassuncao@gmail.com>
2022-06-30 00:51:02 -0400

    Criação da tela Splash Screen

---
+++ app/src/main/java/com/icet/restaurante/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.icet.restaurante
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+
+    }
+}
